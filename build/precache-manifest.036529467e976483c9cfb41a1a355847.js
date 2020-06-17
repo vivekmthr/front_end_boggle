04526b48b491bbea39a44f0ebdbeeb6632254bf8
vmathu02 <vivek.mathur@tufts.edu>
2020-06-17 11:25:10 +0530

    commit

---
+++ build/precache-manifest.036529467e976483c9cfb41a1a355847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3d5b8eaf4b3616f4f3868c6d8e9798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423c4967a0957a8c4213",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "13e2271b6c402624e051",
+    "url": "/static/css/main.401ee8a0.chunk.css"
+  },
+  {
+    "revision": "423c4967a0957a8c4213",
+    "url": "/static/js/2.3bfbbcc3.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.3bfbbcc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e2271b6c402624e051",
+    "url": "/static/js/main.7ab3b79f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
