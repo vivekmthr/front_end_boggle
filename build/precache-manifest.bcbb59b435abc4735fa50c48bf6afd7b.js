ad21610a7302ebf35e5a838a987a91185ac46ea1
vmathu02 <vivek.mathur@tufts.edu>
2020-06-18 16:35:30 +0530

    ui stuff

---
+++ build/precache-manifest.bcbb59b435abc4735fa50c48bf6afd7b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b370e4218903226b684e0eb6154d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f7fe26209e110ba128f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "322e3bd6a429c2bef3a8",
+    "url": "/static/css/main.dc766d8b.chunk.css"
+  },
+  {
+    "revision": "2f7fe26209e110ba128f",
+    "url": "/static/js/2.5a510bcf.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.5a510bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322e3bd6a429c2bef3a8",
+    "url": "/static/js/main.f1b49717.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "89fdff1b7bee3bbd0d97535a7b080f13",
+    "url": "/static/media/desktop_landing.89fdff1b.png"
+  },
+  {
+    "revision": "4522842cae8ffb89b0ce512c8faa7182",
+    "url": "/static/media/desktop_lobby.4522842c.png"
+  },
+  {
+    "revision": "f5bd3c8e57e5ade23a089e21459621d2",
+    "url": "/static/media/phone_landing.f5bd3c8e.png"
+  },
+  {
+    "revision": "95f9528dd88338e95427281e7ae49177",
+    "url": "/static/media/phone_lobby.95f9528d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a07c9c36edfba221508bf19576e5968.js"
+  "/precache-manifest.bcbb59b435abc4735fa50c48bf6afd7b.js"
 );
 
 self.addEventListener('message', (event) => {
